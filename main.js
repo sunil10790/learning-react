ba613eb662654fe357877ed7ee4b082b383717f1
Sunil <sunil10790@gmail.com>
2018-04-21 11:41:41 +0530

    Use ES6 class component

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+class Coco extends React.Component {
+    render() {
+        return (
+            <h1 className="coco">I am in love with my coco</h1>
+        )
+    }
+}
+
 ReactDOM.render(
-    <h1>I am in love with my coco</h1>,
+    <Coco />,
     document.getElementById('react-container')
 )
