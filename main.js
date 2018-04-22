01a148147dbd95ff8df9aff8686cbf2dea2198bb
Sunil <sunil10790@gmail.com>
2018-04-22 08:48:13 +0530

    Using props with stateless component

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-class Coco extends React.Component {
-    render() {
-        return (
-            <h1 className="coco">I am in love with {this.props.amount}mg of my {this.props.product}</h1>
-        )
-    }
-}
+// class Coco extends React.Component {
+//     render() {
+//         return (
+//             <h1 className="coco">I am in love with {this.props.amount}mg of my {this.props.product}</h1>
+//         )
+//     }
+// }
 
-// const Coco = () => (
-//     <h1 className="coco">I am in love with my coco</h1>
-// )
+const Coco = (props) => (
+    <h1 className="coco">I am in love with my {props.product}</h1>
+)
 
 ReactDOM.render(
     <Coco product="coco" amount={16}/>,
