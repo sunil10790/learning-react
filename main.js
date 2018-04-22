1f750608a208779214043fb7b3c23ad1781a7e76
Sunil <sunil10790@gmail.com>
2018-04-22 08:37:22 +0530

    Using props with class component

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// class Coco extends React.Component {
-//     render() {
-//         return (
-//             <h1 className="coco">I am in love with my coco</h1>
-//         )
-//     }
-// }
+class Coco extends React.Component {
+    render() {
+        return (
+            <h1 className="coco">I am in love with {this.props.amount}mg of my {this.props.product}</h1>
+        )
+    }
+}
 
-const Coco = () => (
-    <h1 className="coco">I am in love with my coco</h1>
-)
+// const Coco = () => (
+//     <h1 className="coco">I am in love with my coco</h1>
+// )
 
 ReactDOM.render(
-    <Coco />,
+    <Coco product="coco" amount={16}/>,
     document.getElementById('react-container')
 )
